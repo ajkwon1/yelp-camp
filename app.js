4de819ce73b9e2c37fa2158170bfe94b32225212
Alex <ajkwon@uci.edu>
2022-10-23 20:07:02 -0700

    add start script

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Oh no, something went wrong"
     res.status(statusCode).render('error', { err })
 })
-
-app.listen(3000, () => {
-    console.log('serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`serving on port ${port}`)
 })
